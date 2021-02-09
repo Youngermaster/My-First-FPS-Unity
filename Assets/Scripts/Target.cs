3851a27f8c29eba09d91eb9cc769fa3226b3c323
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2021-02-08 21:52:04 -0500

    Project finished

---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    public float health = 2f;
+    public void DamageReceived(float damageDealt) {
+        health -= damageDealt;
+        if (health >= 0)
+        {
+            Die();
+        }
+    }
+    void Die() {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/WeaponFire.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponFire : MonoBehaviour
+{
+    public float damageAmount = 1f;
+    public float range = 150f;
+    public Camera playerCamera;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }        
+    }
+
+    void Shoot() {
+        RaycastHit hit;
+        if (Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward, out hit, range))
+        {
+            Target target = hit.transform.GetComponent<Target>();
+            if (target != null)
+            {
+                target.DamageReceived(damageAmount);
+            }
+        }
+    }
+}
